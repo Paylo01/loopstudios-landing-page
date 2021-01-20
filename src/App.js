48880eeb2b828d94972f517217ddc3c64ed8c0c6
Paylo01 <paylo_@outlook.com>
2021-01-20 11:28:51 +0100

    created about us section for desktop users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hero from './components/hero-area/hero.component';
+import AboutUs from './components/about-us/about-us.component';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <AboutUs />
     </div>
   );
 }
