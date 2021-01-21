16f7300bde03ace1c83dc49ab282235fea6b916e
Paylo01 <paylo_@outlook.com>
2021-01-21 11:26:02 +0100

    created creations area for desktop users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/hero-area/hero.component';
 import AboutUs from './components/about-us/about-us.component';
+import Creations from './components/creations/creations.component';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <AboutUs />
+      <Creations />
     </div>
   );
 }
