531b2ef1f1b0288ea9978cb3dcac6399d2e79749
Paylo01 <paylo_@outlook.com>
2021-01-28 10:42:28 +0100

    created hero section for mobile users

--- src/components/hero-area/hero.component.jsx
+++ src/components/hero-area/hero.component.jsx
@@ -5,19 +5,25 @@ import logo from '../../images/logo.svg';
 const Hero = () => {
     return (
         <div className="hero">  
+            <div className="hero_img"></div>
             <div className="header_container">
                 <img className="logo" src={logo}/>
-                <ul className="nav_menu"> 
+                <ul className="nav_menu toggle"> 
                     <li><a href="#">About</a></li>
                     <li><a href="#">Careers</a></li>
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Products</a></li>
                     <li><a href="#">Support</a></li>
                 </ul>
+                <ul className="hamburger_menu">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
             <div className="hero_container">
                 <div className="borders">
-                    <h2 className="hero_title">Immersive<br /> experiences <br /> that deliver</h2>
+                    <h2 className="hero_title">Immersive<br /> experiences <br /> that <span className="break">deliver</span></h2>
                 </div>
             </div>
         </div>
